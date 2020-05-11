ebf114757a18b2e99479c177a80cf67b61ca2883
Temitope Olufohunsi Victor <jimlaps@outlook.com>
2020-05-11 19:44:41 +0100

    sending crs to server for genereating crt

---
+++ mqttclientca.sh
@@ -0,0 +1,16 @@
+export SSHPASS=vanetclients
+cd /etc/certs
+echo "Yup"
+sshpass -e sftp -oBatchMode=no -b - vanetclients@192.168.0.98 << !
+   cd incoming_requests
+   put client.csr
+   bye
+!
+echo "Done"
+sleep 2
+sshpass -e sftp -oBatchMode=no -b - vanetclients@192.168.0.98 << !
+   cd outgoing_certificates
+   get client.crt
+   rm client.crt
+   bye
+!
