3acc8e3d8084c9da21ab2210386c8585b773713d
Temitope Olufohunsi Victor <jimlaps@outlook.com>
2020-05-12 15:58:03 +0100

    revised the ca generation script

--- mqttclientca.sh
+++ mqttclientca.sh
@@ -1,16 +1,16 @@
 export SSHPASS=vanetclients
 cd /etc/certs
-echo "Yup"
+echo "Sending CSR to CA server"
 sshpass -e sftp -oBatchMode=no -b - vanetclients@192.168.0.98 << !
    cd incoming_requests
    put client.csr
    bye
 !
-echo "Done"
-sleep 2
+echo "Receiving CA and client certificates"
+sleep 1
 sshpass -e sftp -oBatchMode=no -b - vanetclients@192.168.0.98 << !
-   cd outgoing_certificates
    get ca.crt
+   cd outgoing_certificates
    get client.crt
    rm client.crt
    bye
