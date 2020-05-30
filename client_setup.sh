e2de35468465824c715caf8c746f3e5113a9e838
Temitope Olufohunsi Victor <jimlaps@outlook.com>
2020-05-30 16:36:49 +0100

    made changes to client setup file

--- client_setup.sh
+++ client_setup.sh
@@ -1,9 +1,15 @@
-
 BLOCKCHAINDIR=/etc/blockchain
 
-if test -f "$BLOCKCHAINDIR"; then
-    sudo mkdir "$BLOCKCHAINDIR"   
+if test -e "$BLOCKCHAINDIR"; then
+    echo "Directory already exists"
+else
+    mkdir "$BLOCKCHAINDIR"
+    echo "Done"
 fi
+
+# Adds executable attribute to scripts
+chmod +x client_csr_key_gen.sh
+chmod +x mqttclientca.sh
 
 # Creates the cleint's key and csr for mqtt connection
 ./client_csr_key_gen.sh
